86b34a1186e090b1b5bf31e435cd91d7947532ed
Steven <steven928@hotmail.fr>
2020-12-14 17:32:09 +0100

    add class player and read new player

--- src/Bowling.java
+++ src/Bowling.java
@@ -1,5 +1,37 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import static java.lang.System.out;
+
 public class Bowling {
+
+    ArrayList<Player> players = new ArrayList<>();
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Veuillez entrer le nom des joueurs à ajouter.");
+        Bowling bowling = new Bowling();
+        
+        while(bowling.addNewPlayer()){
+            System.out.println("\n");
+        }
+        System.out.println("Voici les joueurs:");
+        System.out.println(bowling.players);
+    }
+
+    public boolean addNewPlayer() {
+        Scanner keyboard = new Scanner(System.in);
+        String nom;
+
+        System.out.print("Nom du joueur: ");
+
+        nom = keyboard.nextLine();
+
+        if(nom.equals("")){
+            keyboard.close();
+            return false;
+        }else{
+            players.add(new Player(nom,0));
+            return true;
+        }
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,13 @@
+public class Player {
+    public String nom;
+    public int points;
+
+    public Player(String pNom,int pPoints){
+        nom = pNom;
+        points = pPoints;
+    }
+
+    public String getName(){
+        return nom;
+    }
+}
