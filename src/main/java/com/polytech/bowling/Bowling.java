9486a0c801554a4d880c1aac4af95b9926e67d70
Steven <steven928@hotmail.fr>
2020-12-15 14:49:34 +0100

    javadoc

--- src/main/java/com/polytech/bowling/Bowling.java
+++ src/main/java/com/polytech/bowling/Bowling.java
@@ -3,14 +3,23 @@ package com.polytech.bowling;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Classe Bowling
+ */
 public class Bowling {
 
     private ArrayList<Player> players = new ArrayList<>();
 
+    /**
+     * Récupère la liste de joueurs
+     */
     public ArrayList<Player> getPlayers() {
         return players;
     }
 
+    /**
+     * Fonction main
+     */
     public static void main(String[] args) throws Exception {
         System.out.println("Veuillez entrer le nom des joueurs à ajouter.");
         Bowling bowling = new Bowling();
@@ -23,7 +32,7 @@ public class Bowling {
     }
 
     /**
-     * Ajoute un nouveau joueur en entrée à la liste de joueurs
+     * Ajoute un nouveau joueur en entrée à la liste de joueurs.
      */
     public boolean addNewPlayer() {
         Scanner keyboard = new Scanner(System.in);
