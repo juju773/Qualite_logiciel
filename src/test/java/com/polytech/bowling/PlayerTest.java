9a872750d9ecff8f096905d7a4f8f73f2bfc1710
Lucas Payet <lucaspayet@orange.fr>
2021-01-05 16:22:09 +0100

    Suppression calculatepoint

--- src/test/java/com/polytech/bowling/PlayerTest.java
+++ src/test/java/com/polytech/bowling/PlayerTest.java
@@ -13,20 +13,40 @@ public class PlayerTest {
         Player p = new Player("test"); 
         Player p2 = new Player("test"); 
         //Test des strike en chaines
-        p.addPoints(p.calculatePoint(10,1));
-        assertEquals(10, p.getPoints());
-        p.addPoints(p.calculatePoint(10,1));
-        assertEquals(30, p.getPoints());
-        p.addPoints(p.calculatePoint(10,1));
-        assertEquals(60, p.getPoints());
+        p.getScore().addPoint(10, 0, 1);
+        p.getScore().addPoint(10, 1, 1);
+        p.getScore().addPoint(10, 2, 1);
+        p.getScore().addPoint(10, 3, 1);
+        p.getScore().addPoint(10, 4, 1); //T5
+        p.getScore().addPoint(10, 5, 1);
+        p.getScore().addPoint(10, 6, 1);
+        p.getScore().addPoint(10, 7, 1);
+        p.getScore().addPoint(10, 8, 1);
+        p.getScore().addPoint(10, 9, 1); //T10
+        p.getScore().addPoint(10, 9, 1); //T10 bis
+        p.getScore().addPoint(10, 9, 1); //T10 ter
+        assertEquals(300, p.getScore().getScoreTotal());
 
-        //Test des spare en chaines
-        p2.addPoints(p2.calculatePoint(5,1));
-        p2.addPoints(p2.calculatePoint(5,2)); //1er spare
-        p2.addPoints(p2.calculatePoint(5,1));
-        assertEquals(20,p2.getPoints());
-        p2.addPoints(p2.calculatePoint(5,2)); //2eme spare
-        assertEquals(25,p2.getPoints());
+        p2.getScore().addPoint(9, 0, 1);
+        p2.getScore().addPoint(1, 0, 2);
+        p2.getScore().addPoint(2, 1, 1);
+        p2.getScore().addPoint(0, 1, 2);
+        p2.getScore().addPoint(10, 2, 1);
+        p2.getScore().addPoint(2, 3, 1);
+        p2.getScore().addPoint(3, 3, 2);
+        p2.getScore().addPoint(8, 4, 1); //T5
+        p2.getScore().addPoint(2, 4, 2); //T5
+        p2.getScore().addPoint(10, 5, 1);
+        p2.getScore().addPoint(7, 6, 1);
+        p2.getScore().addPoint(2, 6, 2);
+        p2.getScore().addPoint(1, 7, 1);
+        p2.getScore().addPoint(0, 7, 2);
+        p2.getScore().addPoint(10, 8, 1);
+        p2.getScore().addPoint(8, 9, 1); //T10
+        p2.getScore().addPoint(2, 9, 2); //T10 bis
+        p2.getScore().addPoint(7, 9, 1); //T10 ter
+        assertEquals(120, p2.getScore().getScoreTotal());
+
     }
     
 }
